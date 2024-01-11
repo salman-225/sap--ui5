262ef0e14c01aa2a1ae9eb85ded9e0768fab76f7
salman-225 <149861174+salman-225@users.noreply.github.com>
2024-01-11 15:43:20 +0530

    Delete ODataServices/sample-odata-service directory

--- ODataServices/sample-odata-service/app/testproject/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
-    return AppComponent.extend("testproject.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
--- ODataServices/sample-odata-service/srv/employee-service.cds
+++
@@ -1,5 +0,0 @@
-using {com.sijas.capire.employee as my} from '../db/schema';
-
-service EmployeeService @(_requires : 'authenticated-user') {
-  entity Employee as projection on my.Employee;
-}
